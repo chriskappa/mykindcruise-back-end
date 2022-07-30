791a1698ba186ed91a0acf640e4daad62299526f
chriskappa <chriskagkelis@yahoo.com>
2022-07-30 23:21:03 +0100

    Fixed Response

--- Controllers/mainController.js
+++ Controllers/mainController.js
@@ -97,27 +97,28 @@ const getCategory = (category) => {
         {
           id: 1,
           imageLink:
-            "https://www.lwcholidays.com/app/uploads/2019/05/Luxury-Greece-Holiday-Banner.jpg",
+            "https://www.israel21c.org/wp-content/uploads/2021/03/RCCL_Odyssey_PoolDeck.jpg",
           title:
             "8 Night Mediterranean Cruise Vision of the Seas Friday 14 October 2022 from £538pp ",
         },
         {
           id: 2,
           imageLink:
-            "https://www.chasingthedonkey.com/wp-content/uploads/2021/04/Cleopatra-beach_Turkey_Cleopatra-Island_Depositphotos_265905366_s-2019-900x600.jpeg",
+            "https://i.guim.co.uk/img/media/8eace687268eeda921642581623074a3ff64c42f/1219_1066_2958_1775/master/2958.jpg?width=1200&quality=85&auto=format&fit=max&s=119b802505d43141db927a8c90b4d14b",
           title:
             "3 Night Europe Cruise MSC Virtuosa SAturday 24 September 2022 from £306pp",
         },
         {
           id: 3,
           imageLink:
-            "https://purewows3.imgix.net/images/articles/2018_05/nyc_yellow_taxi_in_times_square_hero.jpg?auto=format,compress&cs=strip",
+            "https://www.seabourn.com/content/dam/sbn/marketing-assets/deals/destination/se2025ebs-destination-1800x800.jpg.image.1008.754.low.jpg",
           title:
             "7 Night Europe Cruise Wonder of the Seas Thursday 13 October 2022 from £776pp",
         },
         {
           id: 4,
-          imageLink: "https://www.wasatchpeaks.com/hubfs/Cruise2.jpg",
+          imageLink:
+            "https://www.kuonistores.co.uk/upload/inspiration/james/cruise-suite/regentheader.jpg",
           title:
             "2 Night Europe Cruise Queen Mary 2 Sunday 16 October 2022 from £295pp",
         },
@@ -135,43 +136,43 @@ const getCategory = (category) => {
       return [
         {
           title: "UK Holidays Guide",
-          body: "New to Cruising or a Seasoned Funseeker?Carnival is fun By MattRoberts from My Kind of Cruise",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam ratione odio facere nes",
           image:
             "https://www.expatica.com/app/uploads/sites/10/2014/05/bank-holidays.jpg",
         },
         {
           title: "Greek Holidays Guide",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam ratione odio facere nesciunt nulla.",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam ratione odio facere nes",
           image:
             "https://media.product.which.co.uk/prod/images/original/gm-a01ae6a6-e6e4-4eff-9392-3c0ab71d1876-lefkada1advice.jpg",
         },
         {
           title: "Do I have to tip or pay gratuities onboard?",
-          body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia voluptatibus omnis minus ducimus ",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam ratione odio facere nes",
           image:
-            "https://www.timeout.com/newyork/news/youre-not-dreamingits-impossible-to-find-a-cab-in-nyc-right-now-061521",
+            "https://media.istockphoto.com/photos/dining-at-the-beach-taverna-picture-id1217869695?k=20&m=1217869695&s=612x612&w=0&h=Faeefm5RrfCj-DWbD8kTUWV0C3G18Po2YBzZdT6Ss28=",
         },
         {
           title: "Travel you can really trust",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dicta sint vel saepe similique ",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam ratione odio facere nes",
           image:
             "https://content.tui.co.uk/adamtui/2017_9/6_9/21c10406-d87a-456a-841a-a7e60095f27a/shutterstock_631736717WebOriginalCompressed.jpg?i10c=img.resize(width:658);img.crop(width:658%2Cheight:370)",
         },
         {
           title: "What's a Cabin Category?",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, nesciunt.",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam ratione odio facere nes",
           image:
             "https://us.123rf.com/450wm/dimbar76/dimbar761709/dimbar76170900323/87252898-christmas-decoration-of-the-street-kuznetsk-bridge-in-moscow.jpg?ver=6",
         },
         {
           title: "How do I know which cruise terminal i am saili...",
-          body: "New to Cruising or a Seasoned Funseeker?Carnival is fun By MattRoberts from My Kind of Cruise",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam ratione odio facere nes",
           image:
             "https://assets.traveltriangle.com/blog/wp-content/uploads/2020/01/tahiti1.jpg",
         },
         {
           title: "UK Holidays Guide",
-          body: "New to Cruising or a Seasoned Funseeker?Carnival is fun By MattRoberts from My Kind of Cruise",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam ratione odio facere nes",
           image:
             "https://res.cloudinary.com/lastminute/image/upload/w_500,h_350,c_fill,q_auto,f_auto,dpr_2.0/c_limit,w_768,h_999999/v1649408416/Beach-Pool_View_eimpgu.jpg",
         },
